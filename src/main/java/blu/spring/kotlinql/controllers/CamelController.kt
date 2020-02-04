56262ebf1cfcf9c47091cb0d080b8cb04e2f0295
Luke Petzer <lukepetzerint2@gmail.com>
2020-02-04 14:49:39 +0200

    Changed to defaultEndpoint

--- src/main/java/blu/spring/kotlinql/controllers/CamelController.kt
+++ src/main/java/blu/spring/kotlinql/controllers/CamelController.kt
@@ -1,16 +1,17 @@
 package blu.spring.kotlinql.controllers
 
+import org.apache.camel.Produce
 import org.apache.camel.ProducerTemplate
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Suppress("UNCHECKED_CAST")
 @RestController
-class CamelController(private val producerTemplate: ProducerTemplate) {
+class CamelController(@Produce(uri = "direct:start") private val producerTemplate: ProducerTemplate) {
 
     @RequestMapping("/")
     fun startCamel() {
-        producerTemplate.sendBody("direct:start", "")
+        producerTemplate.sendBody(producerTemplate.defaultEndpoint, "")
     }
 
 }
